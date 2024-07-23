66bb8ad8dea310b4a09a26e1bab18fffd13a85a7
Vaianai <165524107+Vaianai@users.noreply.github.com>
2024-07-23 22:19:36 +0200

    Delete Collections.js

--- Collections.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const gridItems = document.querySelectorAll('.grid-item');
-
-    gridItems.forEach(item => {
-        item.addEventListener('click', () => {
-            item.classList.toggle('selected');
-        });
-    });
-});
